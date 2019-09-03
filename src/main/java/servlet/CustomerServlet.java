a5a08bedb6ddbd2d5f40b465b7a6ec192149cf73
Mikhail Sundukov <fatumepta-git@yandex.ru>
2019-09-03 19:50:24 +0300

    Web task 4 (Init commit)

---
+++ src/main/java/servlet/CustomerServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import com.google.gson.Gson;
+import service.CarService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("CustomerServlet");
+//        Gson gson = new Gson();
+//        String json = gson.toJson(CarService.getInstance().getAllCars());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+    }
+}
---
+++ src/main/java/servlet/DailyReportServlet.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import service.DailyReportService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DailyReportServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("DailyReportServlet");
+//        if (req.getPathInfo().contains("all")) {
+//            DailyReportService.getInstance().getAllDailyReports();
+//        } else if (req.getPathInfo().contains("last")) {
+//            DailyReportService.getInstance().getLastReport();
+//        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doDelete(req, resp);
+    }
+}
